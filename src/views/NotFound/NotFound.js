8d6a569e6095972cb4c284a6c689856405356b20
Onwuzor Victor <onwuzorvictor@outlook.com>
2023-09-04 09:52:57 +0200

    ft: add 404 page

---
+++ src/views/NotFound/NotFound.js
@@ -0,0 +1,14 @@
+import { Typography } from 'components/Typography';
+
+import { ContentContainer } from './NotFound.styles';
+
+export function NotFound() {
+  return (
+    <ContentContainer>
+      <Typography variant="headline">Not Found</Typography>
+      <Typography variant="smallHeadline">
+        Whoops... This page doesn&apos;t exist
+      </Typography>
+    </ContentContainer>
+  );
+}
---
+++ src/views/NotFound/NotFound.styles.js
@@ -0,0 +1,9 @@
+import { styled } from '@mui/material/styles';
+
+export const ContentContainer = styled('div')(() => ({
+  height: '100vh',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  flexDirection: 'column',
+}));
