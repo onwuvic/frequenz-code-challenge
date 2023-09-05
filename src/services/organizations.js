9142c0f044d0c9e83fa5a682de3b392271876722
Onwuzor Victor <onwuzorvictor@outlook.com>
2023-09-05 16:36:38 +0200

    feat: capture more error cases

--- src/services/organizations.js
+++ src/services/organizations.js
@@ -5,7 +5,10 @@ export function useOrganizationsQuery() {
     const response = await fetch('https://api.github.com/organizations');
     const data = await response.json();
     if (!response.ok) {
-      throw new Error(data.message);
+      if (response.status === 403) {
+        throw new Error('You have reach your API rate limit. Try again later.');
+      }
+      throw new Error('Something went wrong');
     }
     return data;
   };
@@ -14,6 +17,60 @@ export function useOrganizationsQuery() {
     queryKey: ['organizations'],
     queryFn: fetchOrganizations,
   });
+
+  return result;
+}
+
+export function useLazyGetOrganizationQuery(name) {
+  const fetchOrganization = async (name) => {
+    const response = await fetch(`https://api.github.com/orgs/${name}`);
+    const data = await response.json();
+    if (!response.ok) {
+      if (response.status === 404) {
+        throw new Error('Organization not found');
+      }
+      if (response.status === 403) {
+        throw new Error('You have reach your API rate limit. Try again later.');
+      }
+      throw new Error('Something went wrong');
+    }
+    return data;
+  };
+
+  const result = useQuery(
+    ['organization', name],
+    () => fetchOrganization(name),
+    {
+      enabled: !!name,
+    }
+  );
+
+  return result;
+}
+
+export function useLazyGetOrganizationReposQuery(name) {
+  const fetchOrganizationRepos = async (name) => {
+    const response = await fetch(`https://api.github.com/orgs/${name}/repos`);
+    const data = await response.json();
+    if (!response.ok) {
+      if (response.status === 404) {
+        throw new Error('Repositories not found');
+      }
+      if (response.status === 403) {
+        throw new Error('You have reach your API rate limit. Try again later.');
+      }
+      throw new Error('Something went wrong');
+    }
+    return data;
+  };
+
+  const result = useQuery(
+    ['repositories', name],
+    () => fetchOrganizationRepos(name),
+    {
+      enabled: !!name,
+    }
+  );
 
   return result;
 }
