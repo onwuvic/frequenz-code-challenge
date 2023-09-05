09270384ce859fdad40d558113d403d1130207e3
Onwuzor Victor <onwuzorvictor@outlook.com>
2023-09-05 16:29:10 +0200

    feat: temporary remove dark and light theme function

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,39 +1,17 @@
+import { Link } from 'react-router-dom';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import IconButton from '@mui/material/IconButton';
-import LightModeIcon from '@mui/icons-material/LightMode';
-import DarkModeIcon from '@mui/icons-material/DarkMode';
-import { useTheme } from '@mui/material/styles';
-import { Link } from 'react-router-dom';
-
-import { useThemeMode } from 'hooks';
 
 export function Nav() {
-  const theme = useTheme();
-  const { toggleThemeMode } = useThemeMode();
-
   return (
     <AppBar component="nav">
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          <Link to="/" sx={{ color: 'white'}}>GIT-ALT</Link>
+          <Link to="/" style={{ textDecoration: 'none', color: 'white' }}>
+              Repofinder
+          </Link>
         </Typography>
-        <Box onClick={toggleThemeMode}>
-          {theme.palette.mode === 'dark' ? (
-            <IconButton aria-label="dark-mode">
-              <DarkModeIcon />
-            </IconButton>
-          ) : (
-            <IconButton
-              aria-label="light-mode"
-              sx={{ color: theme.palette.common.white }}
-            >
-              <LightModeIcon />
-            </IconButton>
-          )}
-        </Box>
       </Toolbar>
     </AppBar>
   );
--- src/hooks/ThemeModeProvider.js
+++ src/hooks/ThemeModeProvider.js
@@ -27,25 +27,6 @@ export function ThemeModeProvider({ children }) {
           palette: {
             ...theme.palette,
             mode,
-            // ...(mode === 'dark' && {
-            //   divider: 'rgba(255, 255, 255, 0.12)',
-            //   text: {
-            //     primary: '#fff',
-            //     secondary: 'rgba(255, 255, 255, 0.7)',
-            //     disabled: 'rgba(255, 255, 255, 0.5)',
-            //   },
-            //   action: {
-            //     active: '#fff',
-            //     hover: 'rgba(255, 255, 255, 0.08)',
-            //     selected: 'rgba(255, 255, 255, 0.16)',
-            //     disabled: 'rgba(255, 255, 255, 0.3)',
-            //     disabledBackground: 'rgba(255, 255, 255, 0.12)',
-            //   },
-            //   background: {
-            //     default: '#121212',
-            //     paper: '#333',
-            //   },
-            // }),
           },
         }}
       >
